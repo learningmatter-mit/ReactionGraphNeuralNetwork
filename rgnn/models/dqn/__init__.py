ba1ce0e668549dee2c6e6a56f6ba81eb3d533cae
HojeChun <hoje.chun316@gmail.com>
2024-05-30 05:04:21 +0900

    `TNet` typo

--- rgnn/models/dqn/__init__.py
+++ rgnn/models/dqn/__init__.py
@@ -2,6 +2,6 @@ from .base import BaseDQN
 from .dqn import ReactionDQN
 from .dqn_v2 import ReactionDQN2
 from .reaction_p import PNet
-from .time import T_Net
+from .time import TNet
 
 __all__ = ["BaseDQN", "ReactionDQN", "ReactionDQN2", "PNet", "TNet"]
