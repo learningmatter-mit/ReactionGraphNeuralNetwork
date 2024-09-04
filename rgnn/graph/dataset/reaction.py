358e2453956011fd46cfa278f510ddb52d58640a
hojechun <hoje.chun316@gmail.com>
2024-09-04 14:55:43 +0900

    reaction dataset

--- rgnn/graph/dataset/reaction.py
+++ rgnn/graph/dataset/reaction.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset as TorchDataset
 from tqdm import tqdm
 
@@ -16,15 +17,34 @@ class ReactionDataset(TorchDataset):
         return self.data_list[idx]
 
 
-def make_dataset(traj_reaction, traj_product, E_r, E_p, E_s, freq):
+def make_dataset(traj_reaction, traj_product, E_r, E_p, Ea, freq, **kwargs):
+    """
+    Create a ReactionDataset from reaction and product trajectories, energies, and frequencies.
+
+    Parameters:
+    traj_reaction (list): List of reaction trajectories (ASE atoms objects).
+    traj_product (list): List of product trajectories (ASE atoms objects).
+    E_r (list): Energies of reactants.
+    E_p (list): Energies of products.
+    Ea (list): Activation energies (energy barriers).
+    freq (list): Frequencies associated with the trajectories.
+    kwargs (dict): Additional attributes to add to the dataset. Each key should be a tuple where the first 
+                   element is a list of values and the second element is the dtype.
+
+    Returns:
+    ReactionDataset: A dataset containing reaction and product data.
+    """
     dataset_list = []
     for i in tqdm(range(len(traj_reaction))):
         data = ReactionGraph.from_ase(traj_reaction[i],
                                       traj_product[i],
-                                      barrier=E_s[i],
+                                      barrier=Ea[i],
                                       freq=freq[i],
                                       energy_i=E_r[i],
                                       energy_f=E_p[i])
+        if kwargs is not None:
+            for key, (values, dtype) in kwargs.items():
+                setattr(data, key, torch.tensor([values[i]], dtype=dtype))
         dataset_list.append(data)
 
     dataset = ReactionDataset(dataset_list)
