e52c8b2b5c7d984c41a87a2ce57fa4f3a162179c
hojechun <hojechun@mit.edu>
2025-09-05 15:58:58 -0400

    `dqn_kmc`

--- rgnn/models/dqn/__init__.py
+++ rgnn/models/dqn/__init__.py
@@ -1,7 +1,7 @@
 from .base import BaseDQN
 from .dqn import ReactionDQN
-from .dqn_v2 import ReactionDQN2
+from .dqn_v2 import ReactionDQN2, ReactionDQN3, ReactionDQN4
 from .reaction_p import PNet
 from .time import TNet
 
-__all__ = ["BaseDQN", "ReactionDQN", "ReactionDQN2", "PNet", "TNet"]
+__all__ = ["BaseDQN", "ReactionDQN", "ReactionDQN2", "ReactionDQN3", "ReactionDQN4", "PNet", "TNet"]
--- rgnn/models/dqn/dqn_v2.py
+++ rgnn/models/dqn/dqn_v2.py
@@ -217,6 +217,7 @@ class ReactionDQN2(BaseDQN):
 @registry.register_model("dqn_v3")
 class ReactionDQN3(BaseDQN):
     """DQN model based on the reaction model.
+    Difference between v2 and v3 is that v3 uses threshold for clipping.
 
     Args:
         reaction_model (BaseReactionModel): The reaction model.
@@ -428,6 +429,132 @@ class ReactionDQN3(BaseDQN):
             q_2 = torch.sum(q_2_i * elem_chempot_tensor, dim=-1, keepdim=True)
             rl_q = q_0 + q_1 * kT + q_2
 
+        outputs[K.rl_q] = rl_q.squeeze(-1)  # (N,)
+
+        return outputs
+
+
+@registry.register_model("dqn_kmc")
+class ReactionDQN4(BaseDQN):
+    """DQN model based on the reaction model.
+
+    Args:
+        reaction_model (BaseReactionModel): The reaction model.
+        N_emb (int, optional): The embedding dimension. Defaults to 16.
+        N_feat (int, optional): The hidden dimension. Defaults to 32.
+        dropout_rate (float, optional): The dropout rate. Defaults to 0.15.
+        canonical (bool, optional): Whether to use the canonical model. Defaults to False.
+    """
+
+    def __init__(
+        self,
+        reaction_model: BaseReactionModel,
+        N_emb: int = 16,
+        N_feat: int = 32,
+        dropout_rate: float = 0.15,
+        canonical: bool = False,
+        mean_mu: Dict[str, float] | None = None,
+        max_mu: Dict[str, float] | None = None,
+    ):
+        super().__init__(reaction_model)
+        self.N_feat = N_feat
+        self.N_emb = N_emb
+        self.dropout_rate = dropout_rate
+        self.canonical = canonical
+        self.mean_mu = mean_mu
+        self.max_mu = max_mu
+        n_input = N_emb + 2 if canonical else N_emb + len(reaction_model.atomic_numbers) + 1
+        self.Q_emb = MLP(
+            n_input=reaction_model.reaction_feat + 1,
+            n_output=N_emb,
+            hidden_layers=(N_emb,),
+            activation="leaky_relu",
+            w_init="xavier_uniform",
+            b_init="zeros",
+            dropout_rate=self.dropout_rate,
+        )
+        self.Q_output = MLP(
+            n_input=n_input,
+            n_output=1,
+            hidden_layers=(N_feat, N_feat),
+            activation="leaky_relu",
+            w_init="xavier_uniform",
+            b_init="zeros",
+            dropout_rate=self.dropout_rate,
+        )
+        self.Q_emb.reset_parameters()
+        self.Q_output.reset_parameters()
+
+    def set_chempot_stats(self, chempot_stats):
+        self.mean_mu = chempot_stats["mean_mu"]
+        self.max_mu = chempot_stats["max_mu"]
+
+    def forward(self, data: DataDict, q_params) -> OutputDict:
+        """Forward pass of the model.
+
+        Args:
+            data (DataDict): The input data (or batch). Can be dict or :class:`aml.data.AtomsGraph`.
+
+        Returns:
+            OutputDict: The dict of computed outputs.
+        """
+
+        results = self.get_q(
+            data, 
+            q_params["temperature"] * self.kb,
+            q_params["alpha"], 
+            q_params["beta"], 
+            q_params["dqn"], 
+        )
+        return results
+
+    def get_q(
+        self,
+        data: DataDict,
+        kT: Tensor | float,
+        alpha=0.0,
+        beta=1.0,
+        dqn=False,
+    ) -> OutputDict:
+        """Calculate the Q for a given reaction graph
+
+        Args:
+            data (DataDict): Reaction graph
+            kT (float): kT (eV)
+            alpha (float, optional): kinetic part of the q. Defaults to 0.0.
+            beta (float, optional): thermodynamic part of the q. Defaults to 1.0.
+
+        Returns:
+            Total Q (Tensor):  Total Q value
+        """
+        outputs = self.reaction_model(data)
+        outputs[K.reaction_features] = data[K.reaction_features]
+        reaction_feat = outputs[K.reaction_features]
+        # Calculate q0
+        q_0 = -1 * alpha * outputs[K.barrier].unsqueeze(-1) - beta * outputs[K.delta_e].unsqueeze(-1)
+        # Calculate q1
+        if alpha != 0.0:
+            if isinstance(kT, Tensor):
+                kT = kT.unsqueeze(-1)
+            elif isinstance(kT, float):
+                kT = torch.as_tensor(kT, dtype=torch.float, device=outputs[K.delta_e].device)
+                kT = kT.repeat(outputs[K.delta_e].shape[0], 1)
+            else:
+                raise TypeError("kT type is wrong or not provided")
+            q_1 = alpha * outputs[K.freq].unsqueeze(-1)
+        else:
+            kT = torch.zeros(outputs[K.delta_e].shape[0], device=outputs[K.delta_e].device).unsqueeze(-1)
+            q_1 = torch.zeros(outputs[K.delta_e].shape[0], device=outputs[K.delta_e].device).unsqueeze(-1)
+        outputs[K.q0] = q_0.squeeze(-1)  # (N,)
+        outputs[K.q1] = q_1.squeeze(-1)  # (N,)
+        if dqn:
+            dqn_feat = F.normalize(torch.cat([q_0, kT * q_1], dim=-1), dim=-1)
+            emb = self.Q_emb(reaction_feat)
+            total_feat = torch.cat([dqn_feat, emb], dim=-1)
+            outputs[K.dqn_feat] = total_feat
+            rl_q = self.Q_output(total_feat)
+        else:
+            rl_q = q_0 + q_1 * kT
         outputs[K.rl_q] = rl_q.squeeze(-1)  # (N,)
 
         return outputs
